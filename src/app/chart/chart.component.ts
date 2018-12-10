9fe5a380697b95bf25bde9f0df6e16ae0ff0e574
Patnaik <sarthak.patnaik@nokia.com>
2018-12-10 11:16:18 +0530

    New changes

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { single } from './data';
+import { Component, OnInit, Input } from '@angular/core';
+// import { single } from './data';
+import {
+  single,
+  multi,
+  bubble,
+  treemap,
+  timelineFilterBarData,
+  fiscalYearReport
+} from '../mockData/chartData';
 
 @Component({
   selector: 'app-chart',
@@ -8,7 +16,7 @@ import { single } from './data';
 })
 export class ChartComponent implements OnInit {
 
-  single: any[];
+ /* single: any[];
   multi: any[];
 
   view: any[] = [600, 350];
@@ -28,9 +36,195 @@ export class ChartComponent implements OnInit {
   };
 
   scheme = 'night';
+*/
+
+
+  theme = 'light';
+  @Input() chartType: string;
+  chartGroups: any[];
+  chart: any;
+  realTimeData = false;
+  countries: any[];
+  single: any[];
+  multi: any[];
+  fiscalYearReport: any[];
+  dateData: any[];
+  dateDataWithRange: any[];
+  calendarData: any[];
+  statusData: any[];
+  sparklineData: any[];
+  timelineFilterBarData: any[];
+  graph: { links: any[]; nodes: any[] };
+  bubble: any;
+  linearScale = false;
+  range = false;
+
+  @Input() view: any[];
+  width = 700;
+  height = 300;
+  fitContainer = false;
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  legendTitle = 'Legend';
+  legendPosition = 'right';
+  showXAxisLabel = true;
+  tooltipDisabled = false;
+  xAxisLabel = 'Country';
+  showYAxisLabel = true;
+  yAxisLabel = 'GDP Per Capita';
+  showGridLines = true;
+  innerPadding = '10%';
+  barPadding = 8;
+  groupPadding = 16;
+  roundDomains = false;
+  maxRadius = 10;
+  minRadius = 3;
+  showSeriesOnHover = true;
+  roundEdges = true;
+  animations = true;
+  xScaleMin: any;
+  xScaleMax: any;
+  yScaleMin: number;
+  yScaleMax: number;
+  showDataLabel = false;
+
+  /*curves = {
+    Basis: shape.curveBasis,
+    'Basis Closed': shape.curveBasisClosed,
+    Bundle: shape.curveBundle.beta(1),
+    Cardinal: shape.curveCardinal,
+    'Cardinal Closed': shape.curveCardinalClosed,
+    'Catmull Rom': shape.curveCatmullRom,
+    'Catmull Rom Closed': shape.curveCatmullRomClosed,
+    Linear: shape.curveLinear,
+    'Linear Closed': shape.curveLinearClosed,
+    'Monotone X': shape.curveMonotoneX,
+    'Monotone Y': shape.curveMonotoneY,
+    Natural: shape.curveNatural,
+    Step: shape.curveStep,
+    'Step After': shape.curveStepAfter,
+    'Step Before': shape.curveStepBefore,
+    default: shape.curveLinear
+  };
+
+  // line interpolation
+  curveType = 'Linear';
+  curve: any = this.curves[this.curveType];
+  interpolationTypes = [
+    'Basis',
+    'Bundle',
+    'Cardinal',
+    'Catmull Rom',
+    'Linear',
+    'Monotone X',
+    'Monotone Y',
+    'Natural',
+    'Step',
+    'Step After',
+    'Step Before'
+  ];
+
+  closedCurveType: string = 'Linear Closed';
+  closedCurve: any = this.curves[this.closedCurveType];
+  closedInterpolationTypes = ['Basis Closed', 'Cardinal Closed', 'Catmull Rom Closed', 'Linear Closed'];
+*/
+  colorSets: any;
+  colorScheme = {
+    domain: ['#781DA0', '#0065AB', '#00968B', '#AAAAAA']
+  };
+  schemeType = 'ordinal';
+  selectedColorScheme: string;
+  rangeFillOpacity = 0.15;
+
+  // Override colors for certain values
+  // customColors: any[] = [
+  //   {
+  //     name: 'Germany',
+  //     value: '#0000ff'
+  //   }
+  // ];
+
+  // pie
+  showLabels = true;
+  explodeSlices = false;
+  doughnut = false;
+  arcWidth = 0.25;
+
+  // line, area
+  autoScale = true;
+  timeline = false;
+
+  // margin
+  margin = false;
+  marginTop = 40;
+  marginRight = 40;
+  marginBottom = 40;
+  marginLeft = 40;
+
+  // gauge
+  gaugeMin = 0;
+  gaugeMax = 100;
+  gaugeLargeSegments = 10;
+  gaugeSmallSegments = 5;
+  gaugeTextValue = '';
+  gaugeUnits = 'alerts';
+  gaugeAngleSpan = 240;
+  gaugeStartAngle = -120;
+  gaugeShowAxis = true;
+  gaugeValue = 50; // linear gauge value
+  gaugePreviousValue = 70;
+
+  // heatmap
+  heatmapMin = 0;
+  heatmapMax = 50000;
+
+  // Combo Chart
+  // barChart: any[] = barChart;
+  // lineChartSeries: any[] = lineChartSeries;
+  lineChartScheme = {
+    name: 'coolthree',
+    selectable: true,
+    group: 'Ordinal',
+    domain: ['#01579b', '#7aa3e5', '#a8385d', '#00bfa5']
+  };
+
+  comboBarScheme = {
+    name: 'singleLightBlue',
+    selectable: true,
+    group: 'Ordinal',
+    domain: ['#01579b']
+  };
+
+  showRightYAxisLabel = true;
+  yAxisLabelRight = 'Utilization';
+
+  // demos
+  totalSales = 0;
+  salePrice = 100;
+  personnelCost = 100;
+
+  mathText = '3 - 1.5*sin(x) + cos(2*x) - 1.5*abs(cos(x))';
+  mathFunction: (o: any) => any;
+
+  treemap: any[];
+  treemapPath: any[] = [];
+  sumBy = 'Size';
+
+  // Reference lines
+  showRefLines = true;
+  showRefLabels = true;
+
+  // Supports any number of reference lines.
+  refLines = [{ value: 42500, name: 'Maximum' }, { value: 37750, name: 'Average' }, { value: 33000, name: 'Minimum' }];
+
+
 
   constructor() {
-    Object.assign(this, { single });
+    Object.assign(this, { single, multi});
   }
 
   onSelect(event) {
